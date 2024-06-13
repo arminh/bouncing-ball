2588c535a4b1661bb2200f898a0cc1b7a2628dfb
Armin Hutzler <arminhutzler@gmx.at>
2024-06-13 13:05:39 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "mbo6j8jc9q";
+export const siteCode = "p60426e6pw";
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
